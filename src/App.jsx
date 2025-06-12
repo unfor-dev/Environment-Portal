949dae37fc8f807ff61a29675988e9f989c05fa1
unfor-dev <hamzayevasadulloh2005@gmail.com>
2025-06-12 22:52:03 +0500

    Adding project starter

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Experience } from "./components/Experience";
+
+function App() {
+  return (
+      <Experience />
+  );
+}
+
+export default App;
---
+++ src/components/Experience.jsx
@@ -0,0 +1,7 @@
+export const Experience = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
