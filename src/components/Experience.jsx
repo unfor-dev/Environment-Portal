3d863e4bd7f28a92e1f45a3f7a6177487ffea2a0
unfor-dev <hamzayevasadulloh2005@gmail.com>
2025-06-13 16:33:22 +0500

    Adding components and styles

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,141 @@
+import { CameraControls, Environment, MeshPortalMaterial, RoundedBox, Text, useCursor, useTexture} from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import { easing } from "maath";
+import { useEffect, useRef, useState } from "react";
+import * as THREE from "three";
+import { Cactoro } from "./Cactoro";
+import { DragonEvolved } from "./Dragon_Evolved";
+import { Fish } from "./Fish";
 export const Experience = () => {
+  const [active, setActive] = useState(null);
+  const [hovered, setHovered] = useState(null);
+  useCursor(hovered);
+  const controlsRef = useRef();
+  const scene = useThree((state) => state.scene);
+
+  useEffect(() => {
+    if (active) {
+      const targetPosition = new THREE.Vector3();
+      scene.getObjectByName(active).getWorldPosition(targetPosition);
+      controlsRef.current.setLookAt(
+        0,
+        0,
+        5,
+        targetPosition.x,
+        targetPosition.y,
+        targetPosition.z,
+        true
+      );
+    } else {
+      controlsRef.current.setLookAt(0, 0, 10, 0, 0, 0, true);
+    }
+  }, [active]);
+
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <ambientLight intensity={0.5} />
+      <Environment preset="sunset" />
+      <CameraControls
+        ref={controlsRef}
+        maxPolarAngle={1}
+        minPolarAngle={1.5}
+
+      />
+      <MonsterStage
+        name="Fish King"
+        color="#38adcf"
+        texture={
+          "textures/1.jpg"
+        }
+        active={active}
+        setActive={setActive}
+        hovered={hovered}
+        setHovered={setHovered}
+      >
+        <Fish scale={0.6} position-y={-1} hovered={hovered === "Fish King"} />
+      </MonsterStage>
+      
+      <MonsterStage
+        texture={"textures/2.jpg"}
+        name="Dragon"
+        color={"#df8d52"}
+        position-x={-2.5}
+        rotation-y={Math.PI / 8}
+        active={active}
+        setActive={setActive}
+        hovered={hovered}
+        setHovered={setHovered}
+      >
+        <DragonEvolved
+          scale={0.5}
+          position-y={-1}
+          hovered={hovered === "Dragon"}
+        />
+      </MonsterStage>
+      <MonsterStage
+        name="Cactoro"
+        color="#739d3c"
+        texture={"textures/3.jpg"}
+        position-x={2.5}
+        rotation-y={-Math.PI / 8}
+        active={active}
+        setActive={setActive}
+        hovered={hovered}
+        setHovered={setHovered}
+      >
+        <Cactoro scale={0.45} position-y={-1} hovered={hovered === "Cactoro"} />
+      </MonsterStage>
+    </>
+  );
+};
+
+const MonsterStage = ({
+  children,
+  texture,
+  name,
+  color,
+  active,
+  setActive,
+  hovered,
+  setHovered,
+  ...props
+}) => {
+  const map = useTexture(texture);
+  const portalMaterial = useRef();
+
+  useFrame((_state, delta) => {
+    const worldOpen = active === name;
+    easing.damp(portalMaterial.current, "blend", worldOpen ? 1 : 0, 0.2, delta);
+  });
+
+  return (
+    <group {...props}>
+      <Text
+        font="fonts/Caprasimo-Regular.ttf"
+        fontSize={0.3}
+        position={[0, -1.3, 0.051]}
+        anchorY={"bottom"}
+      >
+        {name}
+        <meshBasicMaterial color={color} toneMapped={false} />
+      </Text>
+      <RoundedBox
+        name={name}
+        args={[2, 3, 0.1]}
+        onDoubleClick={() => setActive(active === name ? null : name)}
+        onPointerEnter={() => setHovered(name)}
+        onPointerLeave={() => setHovered(null)}
+      >
+        <MeshPortalMaterial ref={portalMaterial} side={THREE.DoubleSide}>
+          <ambientLight intensity={1} />
+          <Environment preset="sunset" />
+          {children}
+          <mesh>
+            <sphereGeometry args={[5, 64, 64]} />
+            <meshStandardMaterial map={map} side={THREE.BackSide} />
+          </mesh>
+        </MeshPortalMaterial>
+      </RoundedBox>
+    </group>
   );
 };
